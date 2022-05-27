ed38ab43f20b234fe6d71afd913a027c036a4cd6
sayuri0709 <53500693+sayuri0709@users.noreply.github.com>
2022-05-27 16:33:54 +0900

    タスクの完了機能

--- src/index.js
+++ src/index.js
@@ -7,10 +7,7 @@ const onClickAdd = () => {
 
   //li生成
   const li = document.createElement("li");
-
-  //div生成
-  const div = document.createElement("div");
-  div.className = "list-row";
+  li.className = "list-row";
 
   //span生成
   const span = document.createElement("span");
@@ -19,8 +16,34 @@ const onClickAdd = () => {
   //button（完了）タグ生成
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
+
   completeButton.addEventListener("click", () => {
-    alert();
+    //押された完了ボタンの親タグ(li)を完了リストから削除
+    deleteFromIncomplete(completeButton.parentNode);
+
+    //完了リストに追加する要素
+    const addTarget = completeButton.parentNode;
+
+    //TODO内容テキストを取得
+    const text = addTarget.firstElementChild.innerText;
+
+    //div以下を初期化
+    addTarget.textContent = null;
+
+    //spanタグ生成
+    const span = document.createElement("span");
+    span.innerText = text;
+
+    //buttonタグ生成
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+
+    //divタグの子要素に各要素を設定
+    addTarget.appendChild(span);
+    addTarget.appendChild(backButton);
+
+    //完了リストに追加
+    document.getElementById("complete-list").appendChild(addTarget);
   });
 
   //button（削除）タグ生成
@@ -28,17 +51,18 @@ const onClickAdd = () => {
   deleatButton.innerText = "削除";
   deleatButton.addEventListener("click", () => {
     //押された削除ボタンの親タグ(li)を未完了リストから削除
-    const deleteTarget = deleatButton.parentNode.parentNode;
-    document.getElementById("incomplete-list").removeChild(deleteTarget);
+    deleteFromIncomplete(deleatButton.parentNode);
   });
 
-  //liタグの子要素にdivを設定
-  li.appendChild(div);
+  //未完了リストから指定の要素を削除
+  const deleteFromIncomplete = (target) => {
+    document.getElementById("incomplete-list").removeChild(target);
+  };
 
   //divタグの子要素に各要素を設定
-  div.appendChild(span);
-  div.appendChild(completeButton);
-  div.appendChild(deleatButton);
+  li.appendChild(span);
+  li.appendChild(completeButton);
+  li.appendChild(deleatButton);
 
   //未完了リストに追加
   document.getElementById("incomplete-list").appendChild(li);
